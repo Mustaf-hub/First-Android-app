24977bada4a068feef810639522df46f7112278b
Mustaf10 <xaviermustaf10@gmail.com>
2026-03-28 14:48:40 +0300

    Personalize preview greeting to 'Mustafa!'
    Update GreetingPreview function to pass "Mustafa!" to Greeting instead of the default "Android".

--- app/src/main/java/com/example/greetingpreview/MainActivity.kt
+++ app/src/main/java/com/example/greetingpreview/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GreetingPreviewTheme {
-        Greeting("Android")
+        Greeting("Mustafa!")
     }
 }
